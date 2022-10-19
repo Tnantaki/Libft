ce93f28ab2333fda0dff28c08ec5db2c1887e988
Tanin Nantakitgosol <tnantaki@z3t4c2.42bangkok.com>
2022-10-19 17:49:27 +0700

    by alias

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tnantaki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tnantaki <tnantaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 17:05:34 by tnantaki          #+#    #+#             */
-/*   Updated: 2022/09/19 14:35:12 by tnantaki         ###   ########.fr       */
+/*   Updated: 2022/10/19 17:48:45 by tnantaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+/* void	*ft_calloc(size_t count, size_t size)
 {
 	void	*str;
 
@@ -22,6 +22,26 @@ void	*ft_calloc(size_t count, size_t size)
 	if (!str)
 		return (NULL);
 	ft_bzero(str, count * size);
+	return (str);
+} */
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*str;
+	char	*s;
+	size_t	i;
+	size_t	len;
+
+	if (count >= SIZE_MAX && size >= SIZE_MAX)
+		return (NULL);
+	len = count * size;
+	str = malloc(len);
+	if (!str)
+		return (NULL);
+	i = 0;
+	s = str;
+	while (i < len)
+		s[i++] = 0;
 	return (str);
 }
 /*
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,12 +6,12 @@
 /*   By: tnantaki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 11:08:15 by tnantaki          #+#    #+#             */
-/*   Updated: 2022/09/16 20:14:42 by tnantaki         ###   ########.fr       */
+/*   Updated: 2022/10/19 08:54:03 by tnantaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+/*
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*dst;
@@ -35,6 +35,32 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	while (i < lens2)
 		dst[lens1 + *j - 1] = s2[i++];
 	dst[lens1 + i] = '\0';
+	return (dst);
+}
+*/
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dst;
+	size_t	lens1;
+	size_t	lens2;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	j = 0;
+	lens1 = ft_strlen(s1);
+	lens2 = ft_strlen(s2);
+	dst = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	if (!dst)
+		return (NULL);
+	while (lens1--)
+		dst[i++] = s1[j++];
+	j = 0;
+	while (lens2--)
+		dst[i++] = s2[j++];
+	dst[i] = '\0';
 	return (dst);
 }
 /*
